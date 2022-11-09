f157004438f641a5cae62626dd6c80e7ad5abf3c
Harry Fox <49591810+hfox1@users.noreply.github.com>
2022-11-09 16:37:26 +0000

    first test passing

--- js_multiples_3_5/multiples.js
+++ js_multiples_3_5/multiples.js
@@ -1,5 +1,14 @@
 const solution = (number) => {
-	return number * 2;
+	let array = [];
+	for (let i = 0; i < number; i++) {
+		if (i % 3 === 0) {
+			array.push(i);
+		}
+		if (i % 5 === 0) {
+			array.push(i);
+		}
+	}
+	return array.reduce((a, b) => a + b, 0);
 };
 
 module.exports = solution;
