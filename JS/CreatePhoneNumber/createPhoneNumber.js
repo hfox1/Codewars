17d7e6adbbf64a0ca3eb19b52149f42339f705c9
Harry Fox <49591810+hfox1@users.noreply.github.com>
2022-11-10 13:56:24 +0000

    all tests passing

--- JS/CreatePhoneNumber/createPhoneNumber.js
+++ JS/CreatePhoneNumber/createPhoneNumber.js
@@ -1,5 +1,11 @@
 const createPhoneNumber = (array) => {
-	return '(111) 111-1111';
+	const e = (index) => {
+		return array[index];
+	};
+
+	return `(${e(0)}${e(1)}${e(2)}) ${e(3)}${e(4)}${e(5)}-${e(6)}${e(7)}${e(
+		8
+	)}${e(9)}`;
 };
 
 module.exports = createPhoneNumber;
