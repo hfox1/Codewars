8c6883d9e724c3c066da2064cdef41f7951b154a
Harry Fox <49591810+hfox1@users.noreply.github.com>
2022-11-11 11:20:46 +0000

    tests written & failing

---
+++ JS/simplePigLatin/pigIt.spec.js
@@ -0,0 +1,12 @@
+const pigIt = require('./pigIt');
+
+describe('pigIt', () => {
+	it('does one word ', () => {
+		expect(pigIt('Pig')).toEqual('igPay');
+	});
+
+	it('does a sentence', () => {
+		expect(pigIt('Pig latin is cool')).toEqual('igPay atinlay siay oolcay');
+		expect(pigIt('This is my string')).toEqual('hisTay siay ymay tringsay');
+	});
+});
