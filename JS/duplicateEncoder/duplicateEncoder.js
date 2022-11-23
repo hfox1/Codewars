3d6f0be85929d9d40b38cf99ee2274852eb8582f
Harry Fox <49591810+hfox1@users.noreply.github.com>
2022-11-23 11:46:06 +0000

    first two tests passing

--- JS/duplicateEncoder/duplicateEncoder.js
+++ JS/duplicateEncoder/duplicateEncoder.js
@@ -1,5 +1,17 @@
-const duplicateEncoder = (string) => {
-	return '(((';
+const duplicateEncoder = (s) => {
+	let string = s.toLowerCase();
+	let chars = string.split('');
+	// console.log(chars);
+	let letters = [];
+	for (var i = 0; i < chars.length; i++) {
+		let toBeRemainingChars = string.split('');
+		toBeRemainingChars.splice(i, 1);
+		console.log(chars, toBeRemainingChars);
+		toBeRemainingChars.includes(chars[i])
+			? letters.push(')')
+			: letters.push('(');
+	}
+	return letters.join('');
 };
 
 module.exports = duplicateEncoder;
