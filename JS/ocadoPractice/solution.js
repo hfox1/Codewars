fac1528e78c82044b96f4f426314047f87783341
Harry Fox <49591810+hfox1@users.noreply.github.com>
2023-01-31 15:58:23 +0000

    third test passing

--- JS/ocadoPractice/solution.js
+++ JS/ocadoPractice/solution.js
@@ -1,5 +1,6 @@
 const solution = (A) => {
 	if (A[1] - A[0] == A[2] - A[1]) return [0, 1, 2];
+	if (A[2] - A[1] == A[3] - A[2]) return [1, 2, 3];
 	else return [];
 };
 
--- JS/ocadoPractice/solution.test.js
+++ JS/ocadoPractice/solution.test.js
@@ -8,8 +8,8 @@ describe('solution', () => {
 	it('gets a single period', () => {
 		expect(solution([1, 3, 5])).toEqual([0, 1, 2]);
 	});
-	it('gets a single period', () => {
-		expect(solution([3, 5, 7])).toEqual([1, 2, 3]);
+	it('gets a second single period', () => {
+		expect(solution([0, 3, 5, 7])).toEqual([1, 2, 3]);
 	});
 
 	xit('gets a single period', () => {});
