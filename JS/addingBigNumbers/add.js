6d4945e0a08b3cbc03f8b011f3f9aacbdc99f6d1
Harry Fox <49591810+hfox1@users.noreply.github.com>
2023-02-06 16:39:00 +0000

    addingBigNumbers all tests passing

--- JS/addingBigNumbers/add.js
+++ JS/addingBigNumbers/add.js
@@ -30,35 +30,27 @@ const add = (a, b) => {
 		sumsArray = [];
 		for (i = longer.length - 1; i >= 0; i--) {
 			sumsArray.unshift(
-				Number(longer[i]) +
-					Number(shorter[i - (longer.length - shorter.length)])
+				+longer[i] + +shorter[i - (longer.length - shorter.length)]
 			);
 		}
-		console.log(sumsArray);
 
-		// do end case since it's an exception to the loop
 		let finalAnswer = '';
 		let digit = 0;
 
 		for (j = sumsArray.length - 1; j >= 0; j--) {
-			let calc = digit + sumsArray[j];
-			if (calc.length > 15 || calc[0] === 0) {
-				digit = calc.split('').shift();
-				partAnswer = calc.join('');
+			let workingTerm = (+digit + sumsArray[j]).toString();
+			if (workingTerm.length > 15) {
+				workingArray = workingTerm.split('');
+				digit = workingArray.shift();
+				partAnswer = workingArray.join('');
 				finalAnswer = partAnswer + finalAnswer;
 			} else {
 				digit = 0;
-				finalAnswer = calc.toString() + finalAnswer;
+				finalAnswer = workingTerm.toString() + finalAnswer;
 			}
 		}
 		return finalAnswer;
 	}
-
-	// looping, starting with the final item
-	// get the first digit with .shift()
-	// add the truncated item to the final string
-	// add the returned digit to the next item
-	// then loop
 };
 module.exports = add;
 
